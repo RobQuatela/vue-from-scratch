69b0b04f7168cddac859e3c203a85d021fc3d029
Rob Quatela <rob.quatela@gmail.com>
2018-09-05 10:53:47 -0400

    initial commit

---
+++ src/webpack.config.dev.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    mode: 'development',
+    entry: [
+        './src/app.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            }
+        ]
+    }
+}
